39df3f18b4c1698cbf68a270aefe82c4dadcd402
guest <guest@guestdemacbook-air-2.local>
2016-07-18 13:20:09 +0800

    Initial Commit

---
+++ 奢易购3.0Tests/___3_0Tests.m
@@ -0,0 +1,39 @@
+//
+//  ___3_0Tests.m
+//  奢易购3.0Tests
+//
+//  Created by guest on 16/7/18.
+//  Copyright © 2016年 cyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___3_0Tests : XCTestCase
+
+@end
+
+@implementation ___3_0Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
